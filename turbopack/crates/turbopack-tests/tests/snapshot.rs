584b24901a6385909df0505faa433da302353199
Sam Poder <sam.r.poder@gmail.com>
2026-07-10 16:50:21 -0700

    [turbopack] Drop unused exports from a CJS module

--- turbopack/crates/turbopack-tests/tests/snapshot.rs
+++ turbopack/crates/turbopack-tests/tests/snapshot.rs
@@ -93,6 +93,8 @@ struct SnapshotOptions {
     #[serde(default)]
     remove_unused_exports: bool,
     #[serde(default)]
+    cjs_tree_shaking: bool,
+    #[serde(default)]
     scope_hoisting: bool,
     #[serde(default)]
     production_chunking: bool,
@@ -134,6 +136,7 @@ impl Default for SnapshotOptions {
             tree_shaking_mode: None,
             remove_unused_imports: false,
             remove_unused_exports: false,
+            cjs_tree_shaking: false,
             scope_hoisting: false,
             production_chunking: false,
             single_chunk: false,
@@ -407,6 +410,7 @@ async fn run_test_operation(resource: RcStr) -> Result<Vc<FileSystemPath>> {
                 })),
                 ignore_dynamic_requests: true,
                 infer_module_side_effects: true,
+                cjs_tree_shaking: options.cjs_tree_shaking,
                 enable_exports_info_inlining: true,
                 enable_rust_react_compiler: options
                     .enable_rust_react_compiler
